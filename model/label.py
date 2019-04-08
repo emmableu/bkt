6c3b1f09848e7b2e6e9f5489e0a47823f934874b
wwang33 <wwang33@ncsu.edu>
2019-04-07 23:17:08 -0400

    16

--- model/label.py
+++ model/label.py
@@ -2,8 +2,9 @@ import pandas as pd
 pd.options.display.max_columns = 30
 col_names  = ['Row','Create', 'Use','Pen', 'Repeat', 'Geometry','Cblock','Note']
 labels = pd.DataFrame(columns = col_names)
+student_num = 16
 # labels = pd.read_csv('generated-data/label2/student0-polygonMakerLab.csv', index_col= 0)
-filename = 
+filename = 'generated-data/label2/student' + str(student_num) + '-polygonMakerLab.csv'
 def fill_labels(row, create, use, pen, repeat, geometry, cblock, note):
     global labels
     for i in range(len(labels.index)):
@@ -21,7 +22,7 @@ def fill_labels(row, create, use, pen, repeat, geometry, cblock, note):
     labels.loc[len(labels)] = new_record
     # labels = labels.append(new_record, ignore_index = True)
     labels = labels.sort_values(['Row']).reset_index(drop=True)
-    labels.to_csv('generated-data/label2/student15-polygonMakerLab.csv')
+    labels.to_csv(filename)
     print(labels)
 
 
@@ -39,7 +40,7 @@ def change_labels(row, create, use, pen, repeat, geometry, cblock, note):
         if labels.ix[i, 'Row'] == row:
             labels.loc[i] = new_record
     labels = labels.sort_values(['Row']).reset_index(drop=True)
-    labels.to_csv('generated-data/label2/student15-polygonMakerLab.csv')
+    labels.to_csv(filename)
     print(labels)
 
 
@@ -49,24 +50,23 @@ def delete_label(row):
         if labels.ix[i, 'Row'] == row:
             labels = labels.drop(labels.index[i])
     labels = labels.sort_values(['Row']).reset_index(drop=True)
-    labels.to_csv('generated-data/label2/student15-polygonMakerLab.csv')
+    labels.to_csv(filename)
     print(labels)
 
 
 
-fill_labels(row = 43085, create =3, use = 3, pen = 3, repeat =4, geometry = 0, cblock = 1, note = '           forward([var=thickness])')
-
-delete_label(42787)
+fill_labels(row = 46601, create =3, use = 4, pen = 3, repeat =6, geometry = 3, cblock = 1, note = '  doRepeat([var=number of sides], ')
+# delete_label(42787)
 #
 print(labels)
 
 
-labels['Anon Student Id'] = map_student.ix[15,  'Anon Student Id']
-labels['New Id'] = 15
-labels.to_csv('generated-data/label2/student15-polygonMakerLab.csv')
+labels['Anon Student Id'] = map_student.ix[student_num,  'Anon Student Id']
+labels['New Id'] = student_num
+labels.to_csv(filename)
 # labels = pd.read_csv('generated-data/label2/student0-polygonMakerLab.csv')
-for i in range(13, 16):
-    labels.ix[i, 'Repeat'] +=1
+for i in range(4, 7):
+    labels.ix[i, 'Create'] =2
 
 # fill_labels(row = 284, create = 0, use = 0, pen = 0, repeat = 0, geometry = 0, cblock = 0, note = "codestart")
 
