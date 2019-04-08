60c7fca543df469f1217fef2fe1a08020e0c5e30
wwang33 <wwang33@ncsu.edu>
2019-04-08 02:20:12 -0400

    23

--- model/label.py
+++ model/label.py
@@ -2,7 +2,7 @@ import pandas as pd
 pd.options.display.max_columns = 30
 col_names  = ['Row','Create', 'Use','Pen', 'Repeat', 'Geometry','Cblock','Note']
 labels = pd.DataFrame(columns = col_names)
-student_num = 22
+student_num = 23
 # labels = pd.read_csv('generated-data/label2/student19-polygonMakerLab.csv', index_col= 0)
 filename = 'generated-data/label2/student' + str(student_num) + '-polygonMakerLab.csv'
 def fill_labels(row, create, use, pen, repeat, geometry, cblock, note):
@@ -55,7 +55,10 @@ def delete_label(row):
 
 
 
-fill_labels(row = 66760, create = 3, use = 4, pen = 3, repeat =6, geometry = 3, cblock = 1, note = 'turn(!!!!!reportQuotient(360, sides))')
+# fill_labels(row = 69233, create = 0, use = 0, pen = 0, repeat = 0, geometry = 0, cblock = 0, note = 'code start')
+
+fill_labels(row = 69621, create = 3, use = 4, pen = 3, repeat = 6, geometry = 3, cblock = 1, note = '  doRepeat([var=Sides], ')
+
 # delete_label66495
 #
 print(labels)
