282b5f4b5fd7d008cc8ff09eaccbac1e326642bc
wwang33 <wwang33@ncsu.edu>
2019-04-09 16:55:58 -0400

    10 squiral

--- model/label.py
+++ model/label.py
@@ -2,7 +2,7 @@ import pandas as pd
 pd.options.display.max_columns = 30
 col_names  = ['Row','Pcreate', 'Puse','Vcreate', 'Vuse','Update', 'Pen', 'Repeat', 'Geometry','Cblock','Note']
 labels = pd.DataFrame(columns = col_names)
-student_num = 4
+student_num = 11
 # labels = pd.read_csv('generated-data/label2/student19-polygonMakerLab.csv', index_col= 0)
 filename = 'generated-data/label2/squiralHW/student' + str(student_num) + '-squiralHW.csv'
 def fill_labels(row, pcreate, puse, vcreate, vuse, update, pen, repeat, geometry, cblock, note):
@@ -60,21 +60,27 @@ def delete_label(row):
     print(labels)
 
 
-#
-fill_labels(row = 19514,
-            pcreate = 1, puse = 1,
-            vcreate = 1,  vuse = 1, update =1,
-            pen = 1, repeat = 8,
-            geometry = 1, cblock = 1,
-            note = '     repeat correct times')
 
+fill_labels(row = 36937,
+            pcreate = 0, puse = 0,
+            vcreate = 0,  vuse = 0, update = 1,
+            pen = 0, repeat = 0,
+            geometry = 0, cblock = 0,
+            note = ' code start')
+
+# fill_labels(row = 31515,
+#             pcreate = 2, puse = 2,
+#             vcreate = 0,  vuse = 0, update = 0,
+#             pen = 1, repeat = 3,
+#             geometry = 0, cblock = 1,
+#             note = '     forward([!!!!!var=Size])')
+#
 
 
 
 delete_label(79521)
 #
 print(labels)
-
 
 labels['Anon Student Id'] = map_student.ix[student_num,  'Anon Student Id']
 labels['New Id'] = student_num
