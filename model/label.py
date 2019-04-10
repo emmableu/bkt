f64d1ab6bfa44ce0832703b1cd1cdbb6db920a55
wwang33 <wwang33@ncsu.edu>
2019-04-10 01:24:13 -0400

    13 squirals

--- model/label.py
+++ model/label.py
@@ -2,7 +2,7 @@ import pandas as pd
 pd.options.display.max_columns = 30
 col_names  = ['Row','Pcreate', 'Puse','Vcreate', 'Vuse','Update', 'Pen', 'Repeat', 'Geometry','Cblock','Note']
 labels = pd.DataFrame(columns = col_names)
-student_num = 19
+student_num = 23
 # labels = pd.read_csv('generated-data/label2/student19-polygonMakerLab.csv', index_col= 0)
 filename = 'generated-data/label2/squiralHW/student' + str(student_num) + '-squiralHW.csv'
 def fill_labels(row, pcreate, puse, vcreate, vuse, update, pen, repeat, geometry, cblock, note):
@@ -60,24 +60,26 @@ def delete_label(row):
     print(labels)
 
 
-fill_labels(row = 56093,
-            pcreate = 0, puse = 0,
-            vcreate = 0,  vuse = 0, update = 0,
-            pen = 1, repeat = 1,
-            geometry = 0, cblock = 1,
-            note = 'dorepeat')
+fill_labels(row = 70390,
+            pcreate = 2, puse = 2,
+            vcreate = 0,  vuse = 0, update = 1,
+            pen = 1, repeat =7 ,
+            geometry = 0, cblock = 0,
+            note = 'correct dochange var ')
 
 
 
-delete_label(56698)
-#
+
+
+delete_label(70762)
+
 print(labels)
 
 labels['Anon Student Id'] = map_student.ix[student_num,  'Anon Student Id']
 labels['New Id'] = student_num
 labels.to_csv(filename)
 # labels = pd.read_csv('generated-data/label2/student0-polygonMakerLab.csv')
-for i in range(1, 4):
+for i in range(2, 4):
     labels.ix[i, 'Repeat'] +=1
 
 # fill_labels(row = 284, create = 0, use = 0, pen = 0, repeat = 0, geometry = 0, cblock = 0, note = "codestart")
